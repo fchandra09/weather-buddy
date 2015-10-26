740e8deb4ead3f7be53e2891aa52271aa0f5a568
margiechubin <margiechubin@Margies-MacBook-Pro.local>
2015-10-26 17:55:02 -0500

    More exciting welcome
    !!

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,5 +1,5 @@
 <?php if (!$this) { exit(header('HTTP/1.0 403 Forbidden')); } ?>
 
 <div class="container">
-    <h2 class="page-header">Welcome!</h2>
+    <h2 class="page-header">Welcome!!</h2>
 </div>
